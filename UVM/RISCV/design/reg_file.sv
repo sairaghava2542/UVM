5cfba8774df5ce4a7ae50724e6c0eb9b58d51afb
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2026-02-17 20:24:30 +0530

    Clean up whitespace in reg_file.sv

--- UVM/RISCV/design/reg_file.sv
+++ UVM/RISCV/design/reg_file.sv
@@ -6,17 +6,13 @@ module regfile(input logic clk,
 					
   logic [31:0] register [31:0]; 	// register file
 	
-
-	
 	// write on falling edge
 	// read on rising edge 
 	
 	// r0 hardwired to 0
 	
-	
   assign rd1 = (a1 != 0 ) ? register[a1] : 0;
   assign rd2 = (a2 != 0 ) ? register[a2] : 0;
-	
 	
 	always_ff @(negedge clk)
       if (we3) register[a3] <= wd3;
