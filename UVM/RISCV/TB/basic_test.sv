6276590f6076ed77455261c7f3e179066a54fabe
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2026-02-17 20:01:24 +0530

    Refactor basic_test.sv for cleaner formatting

--- UVM/RISCV/TB/basic_test.sv
+++ UVM/RISCV/TB/basic_test.sv
@@ -1,17 +1,13 @@
 `timescale 1ns/1ps
-
 module riscv_pip_tb;
-
   // Clock and reset signals
   logic clk;
   logic reset;
-
   // DUT signals
   logic [31:0] PCF;
   logic [31:0] InstrF;
   logic MemWriteM;
   logic [31:0] DataAdrM, WriteDataM , ReadDataM ;
-
   // Instantiate the DUT
   risc_top DUT (
     .clk(clk),
@@ -23,18 +19,14 @@ module riscv_pip_tb;
     .WriteDataM(WriteDataM),
     .ReadDataM(ReadDataM)
   );
-
   // Clock generation
   always #5 clk = ~clk;
-
   // Testbench logic
   initial begin
     // Initialize signals
     clk = 0;
     reset = 1;
     InstrF = 0;
-    
-
     // Reset the DUT
     #15 reset = 0;
     
@@ -63,7 +55,7 @@ module riscv_pip_tb;
                  7'b0010011}; // opcode for ADDI
     
     #10; // Wait for a clock cycle
-    
+  
     // Send SW instruction (put 7 in mem_6)
        InstrF = {7'b0000000,    // imm[11:5] (upper 7 bits of immediate = 0)
                  5'b00110,      // rs2 = x2 (source register containing data to store)
@@ -72,7 +64,6 @@ module riscv_pip_tb;
                  5'b00000,      // imm[4:0] (lower 5 bits of immediate)
                  7'b0100011};   // opcode for SW
                            
-                
       #10; // Wait for a clock cycle
     
     // Send LW instruction (read 7 from mem_6 and put it in x4)
@@ -100,7 +91,6 @@ module riscv_pip_tb;
                  5'b00110,    // rd = x6 (destination register)
                  7'b0010011}; // opcode for ADDI
 
-     
     #10; // Wait for a clock cycle
     
   //  Send ADDI instructions 
@@ -110,7 +100,6 @@ module riscv_pip_tb;
                  5'b00110,    // rd = x6 (destination register)
                  7'b0010011}; // opcode for ADDI
     
-     
     #10; // Wait for a clock cycle
     
   //  Send ADDI instructions 
@@ -119,7 +108,6 @@ module riscv_pip_tb;
                  3'b000,      // funct3 for ADDI
                  5'b00110,    // rd = x6 (destination register)
                  7'b0010011}; // opcode for ADDI
-    
     
      #10; // Wait for a clock cycle
     
@@ -140,9 +128,7 @@ module riscv_pip_tb;
                  7'b0010011}; // opcode for ADDI
     
   $stop ; 
-    
   end
-  
   initial 
   begin
     // Required to dump signals to EPWave
