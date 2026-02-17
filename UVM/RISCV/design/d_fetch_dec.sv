cdcabff5b4acb11d5f2b78a6883e76aa5579b76a
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2026-02-17 20:21:40 +0530

    Fix formatting and clean up d_fetch_dec.sv

--- UVM/RISCV/design/d_fetch_dec.sv
+++ UVM/RISCV/design/d_fetch_dec.sv
@@ -1,8 +1,7 @@
 module IF_ID (input logic clk, reset, clear, enable,
             input logic [31:0] InstrF, PCF, PCPlus4F,
             output logic [31:0] InstrD, PCD, PCPlus4D);
-
-
+	
 always_ff @( posedge clk, posedge reset ) begin
     if (reset) begin // Asynchronous Clear
         InstrD <= 0;
