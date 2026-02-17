e3a1a3ab5b1c1fabfda4750c8df965d7d91d6e1d
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2026-02-17 20:09:11 +0530

    Refactor driver.sv for improved readability

--- UVM/RISCV/TB/driver.sv
+++ UVM/RISCV/TB/driver.sv
@@ -4,23 +4,18 @@ class driver extends uvm_driver #(seq_item);
   seq_item driver_item ;
   virtual intf driver_intf ;
   
-  
  // Constructor  
   function new(string name = "driver" ,uvm_component parent);
     super.new(name,parent);
   endfunction :new
- 
-    
+   
  // Build Phase 
   function void build_phase(uvm_phase phase);  
     super.build_phase(phase);
     if(!(uvm_config_db #(virtual intf)::get(this,"*","risc_intf",driver_intf))) 
    `uvm_error(get_type_name(),"failed to get virtual interface inside Driver class")   
       
-      
-  endfunction :build_phase 
-      
-      
+  endfunction :build_phase     
  // Run Phase 
   task run_phase (uvm_phase phase);   
     super.run_phase(phase);  
@@ -31,8 +26,7 @@ class driver extends uvm_driver #(seq_item);
            seq_item_port.item_done() ;    
          end   
   endtask : run_phase 
-            
-      
+                
  // drive task 
       task drive (seq_item RISC_item) ;
         @(posedge driver_intf.clk) ;
@@ -40,6 +34,4 @@ class driver extends uvm_driver #(seq_item);
 	     driver_intf.InstrF <= RISC_item.InstrF ;
          driver_intf.inst_type <= RISC_item.inst_type ;
       endtask : drive
- 
-  
 endclass : driver
